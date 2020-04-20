94bb9d2d879da0efa6e6697a0567f7a809cfaa27
leoarmelin <leonardo99.armelin@gmail.com>
2020-04-20 11:05:13 -0300

    v1.9

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 
 import dataStyles from '../styles/dataStyles';
 
 export default function Data() {
 	return (
-	  	<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-		    <Text>Data!</Text>
-	  	</View>
+		<SafeAreaView forceInset={{top: 'always'}} style={ dataStyles.container }>
+			<ScrollView>
+				
+			</ScrollView>
+		</SafeAreaView>
 	);
 }
---
+++ src/styles/dataStyles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const dataStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#FFF',
+    },
+
+})
+
+export default dataStyles;
